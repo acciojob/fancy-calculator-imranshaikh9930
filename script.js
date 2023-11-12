0ed6cdd33c7a9eae716937529772ab388637de4c
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-11-12 14:07:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let firstIn= parseInt(prompt());
+let secondIn= parseInt(prompt());
+
+if(firstIn && secondIn){
+	
+firstIn = firstIn * 2;
+
+
+let res =  firstIn + secondIn;
+
+alert(`The final result is: ${res}`);
+	
+}
